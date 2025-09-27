50c71ac2188a91a48ddb02bb05a084b1779f1610
ERWAN <egimenez@et.esiea.fr>
2025-09-27 18:50:25 +0200

    Push V1.0 with Database

---
+++ ContactApp/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactApp
+{
+    internal class Contact
+    {
+        public string nom { get; set; }
+        public string email { get; set; }
+        public int telephone { get; set; }
+
+        public Contact(string nom, string email, int telephone)
+        {
+            this.nom = nom;
+            this.email = email;
+            this.telephone = telephone;
+        }
+
+        public override string ToString()
+        {
+            return "nom : " + nom + "\n" + "email : "  + email + "\n" + "tel : " + telephone;
+        } 
+       
+    }
+}
---
+++ ContactApp/Program.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ContactApp
+{
+internal class Program
+{
+    static void Main(string[] args)
+    {
+            List<Contact> contacts = new List<Contact>();
+
+            int compteurContact = 0;
+
+            while (true)
+            {
+
+                Console.WriteLine("Bienvenue sur l'application de gestion de contact par Erwan le goat");
+
+
+                Console.WriteLine("****** Menu *******");
+
+                Console.WriteLine("1. Ajouter contact");
+                Console.WriteLine("2. Afficher contacts");
+                Console.WriteLine("3. Recherche un contact");
+                Console.WriteLine("4. Modifier un contact");
+                Console.WriteLine("5. Supprimer contact");
+                Console.WriteLine("6. Quitter");
+
+                string menuReponseUti = Console.ReadLine();
+
+
+                
+
+
+                switch (menuReponseUti)
+
+                {
+                    case "1":
+
+                        Console.Write("Veuillez entrer un nom :");
+                        string reponseUtiAjouterNom = Console.ReadLine();
+
+                        Console.Write("Veuillez entrer un email :");
+                        string reponseUtiAjouterEmail = Console.ReadLine();
+
+
+                        Console.Write("Veuillez entrer un Telephone :");
+                        int reponseUtiAjouterTelephone = Convert.ToInt32(Console.ReadLine());
+
+                         contacts.Add(new Contact(reponseUtiAjouterNom, reponseUtiAjouterEmail, reponseUtiAjouterTelephone));
+
+                        compteurContact++;
+
+                        if (contacts.Count > 0 )
+                        {
+                            Console.WriteLine("Utilisateur crée");
+                        }
+
+                        
+                        break;
+
+                    case "2":
+                        Console.WriteLine("On va afficher les contact si dessous : ");
+
+                        foreach (var listeContact in contacts)
+                        {
+                            Console.WriteLine(listeContact);
+                        }
+
+                        Console.Write("Voulez vous revenir au menu principal ?");
+                        string reponseUtiRetourAuMenu = Console.ReadLine();
+
+                        if (reponseUtiRetourAuMenu == "Oui")
+                        {
+                            continue;
+                        }
+                        
+                        break;
+                    case "3":
+                        Console.WriteLine("Recherche de contact ( attention a la Case");
+                        while (true)
+                        {
+                            Console.Write("A l'aide de quoi voulez vous trouver un contact ? \n 1. Nom \n 2. Email \n 3. Telephone \n");
+                            string reponseUtiTypeRecherche = Console.ReadLine();
+
+                            switch (reponseUtiTypeRecherche)
+                            {
+                                case "1":
+                                    Console.Write("Taper le nom de la personne recherchée : ");
+                                    string reponseUtiTypeRechercheNom = Console.ReadLine();
+
+                                    bool contactTrouve = false; 
+
+                                    foreach (var contact in contacts)
+                                    {
+                                        
+                                        if (contact.nom.Contains(reponseUtiTypeRechercheNom))
+                                        {
+                                            Console.WriteLine(contact); 
+                                            contactTrouve = true;
+                                        }
+                                    }
+
+                                    if (!contactTrouve)
+                                    {
+                                        Console.WriteLine("Aucun contact trouvé !");
+                                    }
+                                    break;
+                                
+                                case "2":
+                                    Console.WriteLine("Taper l'email de la personne rechercher");
+                                    string reponseUtiTypeRechercheEmail = Console.ReadLine();
+
+                                    bool contactTrouveEmail = false; 
+
+                                    foreach (var contact in contacts)
+                                    {
+                                        
+                                        if (contact.email.Contains(reponseUtiTypeRechercheEmail))
+                                        {
+                                            Console.WriteLine(contact); 
+                                            contactTrouveEmail = true;
+                                        }
+                                    }
+
+                                    if (!contactTrouveEmail)
+                                    {
+                                        Console.WriteLine("Aucun contact trouvé !");
+                                    }
+                                    break;
+
+                                case "3":
+                                    Console.WriteLine("Taper le telephone la personne rechercher");
+                                    string reponseUtiTypeRechercheTelephone = Console.ReadLine();
+
+                                    bool contactTrouveTelephone = false; 
+
+                                    foreach (var contact in contacts)
+                                    {
+                                        
+                                        if (contact.telephone.ToString().Contains(reponseUtiTypeRechercheTelephone))
+                                        {
+                                            Console.WriteLine(contact); 
+                                            contactTrouveTelephone = true;
+                                        }
+                                    }
+
+                                    if (!contactTrouveTelephone)
+                                    {
+                                        Console.WriteLine("Aucun contact trouvé !");
+                                    }
+                                    break;
+
+                            }
+
+                            Console.Write("Voulez vous refaire une recherche ?");
+                            string reponseUtiNouvelleRecherche = Console.ReadLine();
+
+                        if (reponseUtiNouvelleRecherche == "Non")
+                        {
+                            break;
+                        }
+                        }
+                        break;
+                    case "4":
+                        Console.WriteLine("Modification de contact");
+
+                        while (true)
+                        {
+
+
+                            int numeroContact = 1;
+                            foreach (var listeContact in contacts)
+                            {
+                                Console.WriteLine(numeroContact + ". " + listeContact);
+                                numeroContact++; 
+                            }
+
+                            Console.Write("Taper le numero du contact que vous souhaitez modifier : ");
+                            string reponseUtiModifContact = Console.ReadLine();
+
+                            int numeroChoisi = Convert.ToInt32(reponseUtiModifContact);
+                            int indexDansListe = numeroChoisi - 1;
+
+                            
+                            Console.Write("Entrez le nouveau nom : ");
+                            string reponseUtiModifContactNom = Console.ReadLine();
+
+                            Console.Write("Entrez le nouveau email : ");
+                            string reponseUtiModifContactEmail = Console.ReadLine();
+
+                            Console.Write("Entrez le nouveau telephone : ");
+                            string reponseUtiModifContactTelephone = Console.ReadLine();
+
+                            int reponseUtiModifContactTelephoneConvert = Convert.ToInt32( reponseUtiModifContactTelephone );
+
+
+                            contacts[indexDansListe].nom = reponseUtiModifContactNom;
+                            contacts[indexDansListe].email = reponseUtiModifContactEmail;
+                            contacts[indexDansListe].telephone = reponseUtiModifContactTelephoneConvert;
+                            Console.WriteLine("Contact modifié !");
+
+                            Console.Write("Voulez vous revenir au menu principal ?");
+                            string reponseUtiRetourAuMenuModif = Console.ReadLine();
+
+                            if (reponseUtiRetourAuMenuModif == "Oui")
+                            {
+                                break;
+                            }
+
+
+
+                            break;
+                        }
+
+                        break;
+                    case "5":
+                        Console.WriteLine("Supprimer un contact");
+                        while (true)
+                        {
+                            int numeroContactSupp = 1;
+                            foreach (var listeContact in contacts)
+                            {
+                                Console.WriteLine(numeroContactSupp + ". " + listeContact);
+                                numeroContactSupp++;
+                            }
+
+                            Console.Write("Taper le numero du contact que vous souhaitez supprimer : ");
+                            string reponseUtiSuppContact = Console.ReadLine();
+
+                            int numeroChoisiSupp = Convert.ToInt32(reponseUtiSuppContact);
+                            int indexDansListeSupp = numeroChoisiSupp - 1;
+
+
+                            contacts.RemoveAt(indexDansListeSupp);
+
+                            Console.WriteLine("Contact supprimer !");
+
+                            Console.Write("Voulez vous revenir au menu principal ?");
+                            string reponseUtiRetourAuMenuModif = Console.ReadLine();
+
+                            if (reponseUtiRetourAuMenuModif == "Oui")
+                            {
+                                break;
+                            }
+
+
+
+
+
+
+
+
+                        }
+                        break;
+                }
+
+
+                if (menuReponseUti == "6")
+                {
+                    return;
+                }
+
+            }
+
+
+            
+    }
+}
+}
